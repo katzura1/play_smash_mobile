477fad08360d661be9e7564e7a18c3f1005e65bb
Denny <denny.az45@gmail.com>
2024-10-23 18:56:23 +0700

    feat: prototype splash screen

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:play_smash/shared/theme.dart';
 
 class SplashPage extends StatelessWidget {
@@ -8,7 +9,72 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: Center(),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Animate(
+              effects: const [
+                FadeEffect(
+                  delay: Duration(
+                    milliseconds: 500,
+                  ),
+                  duration: Duration(
+                    milliseconds: 500,
+                  ),
+                ),
+                ScaleEffect(
+                  delay: Duration(
+                    milliseconds: 500,
+                  ),
+                  duration: Duration(
+                    milliseconds: 500,
+                  ),
+                ),
+              ],
+              child: Container(
+                height: 93,
+                width: 195,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/logo_above.png'),
+                  ),
+                ),
+              ),
+            ),
+            Animate(
+              effects: const [
+                FadeEffect(
+                  delay: Duration(
+                    milliseconds: 1000,
+                  ),
+                  duration: Duration(
+                    milliseconds: 500,
+                  ),
+                ),
+                ScaleEffect(
+                  delay: Duration(
+                    milliseconds: 1000,
+                  ),
+                  duration: Duration(
+                    milliseconds: 500,
+                  ),
+                ),
+              ],
+              child: Container(
+                height: 52,
+                width: 246,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/logo_below.png'),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
