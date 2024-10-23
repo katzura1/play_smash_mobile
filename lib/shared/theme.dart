d8a0b7e6772d829afb3047ca99c857c5a0a3efd7
Denny <denny.az45@gmail.com>
2024-10-23 17:31:35 +0700

    feat: first commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//color
+Color whiteColor = const Color(0xffFFFFFF);
+Color primaryColor = const Color(0xff073353);
+Color blackColor = const Color(0xff000000);
+Color grayColor = const Color(0xff998787);
+Color backgroundColor = const Color(0xff073353);
+Color grayBackgroundColor = const Color(0xffD9D9D9);
+
+//font weight
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
+
+//text style
+TextStyle whiteTextStyle = GoogleFonts.roboto(
+  color: whiteColor,
+);
+
+TextStyle primaryTextStyle = GoogleFonts.roboto(
+  color: primaryColor,
+);
+
+TextStyle blackTextStyle = GoogleFonts.roboto(
+  color: blackColor,
+);
+
+TextStyle grayTextStyle = GoogleFonts.roboto(
+  color: grayColor,
+);
