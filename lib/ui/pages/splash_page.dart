0326646072c15f502d3508293c2c05228203fa2e
Denny <denny.az45@gmail.com>
2024-10-23 17:57:24 +0700

    feat: remove default splash screen

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:play_smash/shared/theme.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Center(),
+    );
   }
 }
